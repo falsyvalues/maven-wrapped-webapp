c65497e8295020794da7f787f34abfbaba6d886e
Michał Lipiński <mylith@gmail.com>
2017-02-06 14:52:43 +0100

    Separate dynamic and static part of project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('default', function() {
-  return gulp.src('src/main/webapp/**/*')
+  return gulp.src('src/main/frontend/**/*')
     .pipe(gulp.dest('./target/generated-webapp'));
 });
